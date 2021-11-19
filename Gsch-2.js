9221d41456816faa70ca35a96ae9a97f9464dc72
aethlis <84690253+aethlis@users.noreply.github.com>
2021-11-19 09:47:30 +0900

    updated files
    aa

---
+++ Gsch-2.js
@@ -0,0 +1,12 @@
+const videoFrame = document.querySelector("#videoIFrame")
+
+const videoHeight = videoFrame.height
+
+// console.log(videoHeight)
+
+const videoWidth = "100vw"
+
+videoFrame.style.width = videoWidth
+
+videoFrame.style.height = videoWidth 
+
